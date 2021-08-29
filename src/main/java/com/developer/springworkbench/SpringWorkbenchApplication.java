9d3296d5a8cbe27115e00c5d8fcdaad6e62541d7
618developer <jrsm0713@yahoo.com>
2021-08-28 23:12:36 -0500

    setting up a baseline spring api project

---
+++ src/main/java/com/developer/springworkbench/SpringWorkbenchApplication.java
@@ -0,0 +1,13 @@
+package com.developer.springworkbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorkbenchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWorkbenchApplication.class, args);
+	}
+
+}
